7e3227038c7d07b077cdc288451f06b54ae07f0a
Vasu Bhog <vabhog@microsoft.com>
2022-11-09 15:38:09 -0800

    add boilerplate

---
+++ HttpTrigger1/runDefault.ps1
@@ -0,0 +1,20 @@
+using namespace System.Net
+
+# Trigger binding data passed in via param block
+param($Request)
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell function with SQL Output Binding processed a request."
+
+$req_body = $Request.Body
+
+# Assign the value we want to pass to the SQL Output binding. 
+# The -Name value corresponds to the name property in the function.json for the binding
+Push-OutputBinding -Name products -Value $req_body
+
+# Assign the value to return as the HTTP response. 
+# The -Name value matches the name property in the function.json for the binding
+Push-OutputBinding -Name response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = $req_body
+})
